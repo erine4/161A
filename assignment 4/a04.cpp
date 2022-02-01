dbcc9cd4b9ac84198fd126a302f1c1cc982101bd
erine4 <erin.egloff@pcc.edu>
2022-02-01 14:15:37 -0800

    commentheaders

--- assignment 4/a04.cpp
+++ assignment 4/a04.cpp
@@ -1,63 +1,94 @@
 #include <iostream>
 #include <iomanip>
 #include <cstdlib>
+using namespace std;
 
-
-//const int
+/* const int */
 const double adultPassFree = 100.00;
 const double nonAdultPassFree = 28.00;
 const double adultTicket = 2.50;
 const double nonAdultTicket = 1.25;
 
 int main() {
-    //declare variables
+    /* declare variables */
     char typeFastPass;
     int numberTickets = 0;
     double totalCost = 0.0;
     double needToSpend = 0.0;
 
-    //opening message
-    std::cout << "“Welcome to TriMet Hop Fastpass!" 
-                << std::endl 
-                << std::endl;
+    /* opening message */
+    cout << "Welcome to TriMet Hop Fastpass!" 
+                << endl 
+                << endl;
 
-    //fastPass choices
-    std::cout << "FastPass Ticket: "
-                << std::setfill(' ') 
-                << std::setw(33) 
+    /* fastPass choices */
+    cout << "FastPass Ticket: "
+                << setfill(' ') 
+                << setw(33) 
                 << "Ticket $" 
-                << std::endl;
+                << endl;
 
-    //divider
-    std::cout << std::setfill('-')
-                << std::setw(60)
+    /* divider */
+    cout << std::setfill('-')
+                << setw(60)
                 << " "
-                << std::setfill(' ')
-                << std::endl;
+                << setfill(' ')
+                << endl;
 
-    //adult pass
-    std::cout << "A. Adult Pass (ages 18-64)"
-                << std::setw(16)
+    /* adult pass */
+    cout << "A. Adult Pass (ages 18-64)"
+                << setw(16)
                 << " "
                 << "$2.50"
-                << std::endl;
+                << endl;
 
-    //honored citizen pass
-    std::cout << "H. Honored Citizen (65+, disabilities)"
-                << std::setw(4)
+    /* honored citizen pass */
+    cout << "H. Honored Citizen (65+, disabilities)"
+                << setw(4)
                 << " "
                 << "$1.25"
-                << std::endl;
+                << endl;
     
-    //youth pass
-    std::cout << "Y. Youth Pass (ages 7-17)"
-                << std::setw(17) 
+    /* youth pass */
+    cout << "Y. Youth Pass (ages 7-17)"
+                << setw(17) 
                 << " "
                 << "$1.25"
-                << std::endl;
+                << endl
+                << endl;
+    /* note */
+    cout << "Note: Ride is 2 hours and 30 minutes with each ticket."
+                << endl
+                << endl;
+    
+    /* free ride */
+    cout << "Ride free for the rest of the month after spending $100 with an Adult pass," 
+                << endl
+                << "or $28 with an Honored Citizen or Youth pass!"
+                << endl
+                << endl;
 
+    /* input type of pass */
+    cout << "Please enter the type of pass you wish to purchase (enter A, H, or Y): ";
+        cin >> typeFastPass;
+    
+    /* input number of tickets */
+    cout << "Enter the number of tickets purchased this month: ";
+        cin >> numberTickets;
 
+    /* display number of tickets */
+    cout << "You have purchased " << numberTickets << "tickets!" << endl;
 
+    /* if then statements */
+    if ((typeFastPass == 'A') || (typeFastPass == 'a'))   {
+        totalCost = adultTicket * numberTickets;
+        cout << "You have paid: $" << totalCost << ".";
+    }
+
+    else if ((typeFastPass == 'H') || (typeFastPass == 'h') || (typeFastPass == 'Y') || (typeFastPass == 'y'))    {
+        totalCost = nonAdultTicket * numberTickets;
+        cout << "You have paid: $" << totalCost << ".";
+    }
 
 
 
