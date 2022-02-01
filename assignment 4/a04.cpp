966f713f601577c6c9a3507bab27075ed294841b
erine4 <erin.egloff@pcc.edu>
2022-02-01 12:39:53 -0800

    assignment4

---
+++ assignment 4/a04.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+//const int
+const double adultPassFree = 100.0;
+const double nonAdultPassFree = 28.0;
+const double adultTicket = 2.50;
+const double nonAdultTicket = 1.25;
+
+int main() {
+    //declare variables
+    char typeFastPass;
+    int numberTickets = 0;
+    double totalCost = 0.0;
+    double needToSpend = 0.0;
+
+    //opening message
+    cout << "“Welcome to TriMet Hop Fastpass!" << endl << endl;
+
+    //fastPass choices
+    cout << "FastPass Ticket choices: " 
+        << endl;
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
