d38bc9a99adcd75af764436ed859edbcfba37c32
erine4 <erin.egloff@pcc.edu>
2022-02-06 22:12:57 -0800

    oops

---
+++ assignment 5/a05.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    string textMessage;
+    string wordBFF = "BFF";
+    string wordIDK = "IDK"; 
+    string wordJK = "JK"; 
+    string wordTMI = "TMI"; 
+    string wordTTYL = "TTYL"; 
+
+
+
+
+
+
+    return 0;
+}
