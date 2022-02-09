acba4745e30fb74b37f7d280e97c0bc746990542
erine4 <erin.egloff@pcc.edu>
2022-02-09 12:45:38 -0800

    commentheader

--- assignment 5/a05.cpp
+++ assignment 5/a05.cpp
@@ -1,7 +1,23 @@
+/******************************************************************************
+# Author:           Erin Egloff
+# Assignment:       Assignment 5
+# Date:             02/09/2022
+# Description:      This program will calculate the total price based on their type 
+                    of Hop Fastpass. The user will know the number of tickets purchased, 
+                    how much it will cost, and the cost to qualify for free rides for the 
+                    month (if applicable or not attained).
+# Input:            Str textMessage
+# Output:           "You have purchased ___ tickets!"
+                    "You have paid: $___."
+                    "Spend $___ more to earn free rides for the rest of the month."
+# Sources:          zyLab 5
+******************************************************************************/
+
 #include <iostream>
 using namespace std;
 
 int main() {
+    //declare strings
     string textMessage;
     string wordBFF = "BFF";
     string wordIDK = "IDK"; 
@@ -9,14 +25,17 @@ int main() {
     string wordTMI = "TMI"; 
     string wordTTYL = "TTYL"; 
 
+    //display welcome message
     cout << "Welcome to the Text Message Decoder!" << endl << endl;
     
+    //get user input
     cout <<  "Enter a single line text message: ";
     getline(cin, textMessage);
     
     cout << endl;
     cout << "You entered: " << textMessage << endl;
 
+    //output definitions for abbreviations
     textMessage.find(wordBFF);
     if (textMessage.find(wordBFF) != string::npos)  {
         cout << "BFF: best friend forever" << endl;
@@ -42,6 +61,7 @@ int main() {
         cout << "TTYL: talk to you later" << endl;
     }
 
+    //end of program
     cout << endl;
     cout << "END." << endl;
 
