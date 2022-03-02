66737534738eb35e65415b0112c4b8cabddbe844
erine4 <erin.egloff@pcc.edu>
2022-03-02 11:26:33 -0800

    hard

---
+++ assignment 7/a07.cpp
@@ -0,0 +1,44 @@
+/******************************************************************************
+# Author:           Erin Egloff
+# Assignment:       Assignment #7
+# Date:             March 2, 2022
+# Description:      This program This program will output menu options for the 
+#                   user to choose from. Once an option has been selected, the 
+#                   option and price will be shown and continually added to the 
+#                   total cost. After the user has made all final selections, the 
+#                   total price will be shown, as well as some tip option to 
+#                   choose from. Once the tip option has been selected, the 
+#                   total amount will be shown. 
+# Input:            int userInput, 
+# Output:           
+# Sources:          None
+#******************************************************************************/
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+int main()  {
+    //declare variables
+    int userOption;
+    
+
+    //set precision to 2 decimals
+    cout << fixed << setprecision(2);
+
+    //Welcome message
+    cout << "Welcome to my Coffee Shop!" << endl << endl;
+
+
+
+
+
+
+
+
+    return 0;
+}
+
