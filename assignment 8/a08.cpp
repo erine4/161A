a4656b9a697f783cdf4b0964473850971c1a4507
erine4 <erin.egloff@pcc.edu>
2022-03-07 09:24:35 -0800

    assignment8

---
+++ assignment 8/a08.cpp
@@ -0,0 +1,40 @@
+/******************************************************************************
+# Author:           Erin Egloff
+# Assignment:       Assignment #8
+# Date:             March 7, 2022
+# Description:      This program will output menu options for the 
+#                   user to choose from. Once an option has been selected, the 
+#                   option and price will be shown and continually added to the 
+#                   total cost. After the user has made all final selections, the 
+#                   total price will be shown, as well as some tip options to 
+#                   choose from. Once the tip has been inputted, the 
+#                   total amount will be shown. 
+# Input:            
+# Output:           
+# Sources:          Assignment 8 Google Doc
+#******************************************************************************/
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+
+
+
+
+
+
+
+int main()  {
+
+
+
+
+
+
+
+    return 0;
+}
