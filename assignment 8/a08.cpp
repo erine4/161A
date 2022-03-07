6986215909cf9e0fda24f6c8f3f8a6c90bbf9867
erine4 <erin.egloff@pcc.edu>
2022-03-07 10:47:51 -0800

    quitissue

--- assignment 8/a08.cpp
+++ assignment 8/a08.cpp
@@ -21,16 +21,117 @@
 
 using namespace std;
 
+//Declare constants
+const double costDonut = 4.00;
+const double costMuffin = 4.50;
+const double costPastry = 5.50;
+const double costBagel = 3.75;
+const double costToast = 2.25;
+const double costCoffee = 3.50;
+const double costJuice = 2.50;
+
+void printWelcome () {
+    cout << "Welcome to my Coffee Shop!" << endl << endl;
+}
 
 
+void printMenu () {
+    //Menu options
+    cout << "Please pick an option below:" << endl;
+    cout << "1. Donuts/Muffins/Pastries" << endl;
+    cout << "2. Bagels/Toast" << endl;
+    cout << "3. Coffee/Juice" << endl;
+    cout << "4. Quit" << endl << endl;
+}
 
+int readOption (int input)  {
+    //input checker
+    while ((input < 1) || (input > 4))  {
+        cin.clear();
+        cin.ignore(100, '\n');
+        cout << "Invalid option. Please choose 1-4: ";
+        cin >> input;
+        cout << endl;
+    }
+}
 
+//FIXME: how to add choice
+void printSelection (char choice)  {
 
+    if ((choice == 'D') || (choice == 'd')) {
+        cout << "Donuts added." << endl;
+    }
+    else if ((choice == 'M') || (choice == 'm'))    {
+        cout << "Muffins added." << endl;
+    }
+    else if ((choice == 'P') || (choice == 'p'))    {
+        cout << "Pastries added." << endl;
+    }
+    else if ((choice == 'B') || (choice == 'b'))    {
+        cout << "Bagels added." << endl;
+    }
+    else if ((choice == 'T') || (choice == 't'))    {
+        cout << "Toast added." << endl;
+    }
+    else if ((choice == 'C') || (choice == 'c'))    {
+        cout << "Coffee added." << endl;
+    }
+    else if ((choice == 'J') || (choice == 'j'))    {
+        cout << "Juice added." << endl;
+    }
+}
+
+void printSubMenu (int option)  {
+    
+    if (option == 1)    {
+        cout << "Would you like" << endl;
+        cout << "D: Donuts $" << costDonut << endl;
+        cout << "M: Muffins $" << costMuffin << endl;
+        cout << "P: Pastries $" << costPastry << endl << endl;
+        cout << endl;
+    }
+
+    else if (option == 2)   {
+        cout << "Would you like" << endl;
+        cout << "B: Bagels $" << costBagel << endl;
+        cout << "T: Toast $" << costToast << endl << endl;
+        cout << endl;
+    }
+     
+    else if (option == 3)   {
+        cout << "Would you like" << endl;
+        cout << "C: Coffee $" << costCoffee << endl;
+        cout << "J: Juice $" << costJuice << endl << endl;
+        cout << endl;
+    }
+}
 
 
 int main()  {
+    int userOption;
+    char userChar;
+    int total;
 
-
+    printWelcome();
+    
+    cout << fixed << setprecision(2);
+    
+    while (userOption != 4) {
+        printMenu();
+        cin >> userOption;
+        readOption(userOption);
+        cout << endl;
+    
+        printSubMenu(userOption);
+        cin >> userChar;
+        cout << endl;
+        printSelection(userChar);
+    
+        
+    
+    
+    }
+    
 
 
 
