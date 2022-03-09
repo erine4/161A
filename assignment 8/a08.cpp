c1180ce161fd37c2b9d89626d4e951881a8853ba
erine4 <erin.egloff@pcc.edu>
2022-03-08 21:38:32 -0800

    bouttokermit

--- assignment 8/a08.cpp
+++ assignment 8/a08.cpp
@@ -30,7 +30,103 @@ const double costToast = 2.25;
 const double costCoffee = 3.50;
 const double costJuice = 2.50;
 
-void printWelcome () {
+//Declare functions
+void printWelcomeMessage();
+void printEndMessage();
+void printMenu();
+int printSubMenu();
+int readOption();
+int readOptionChar();
+
+int printSelection (char choice)  {
+    cin >> choice;
+
+    double total = 0.00;
+
+    //choice added
+    if ((choice == 'D') || (choice == 'd')) {   
+        cout << "Donuts added. $";
+        total += costDonut;
+    }
+    else if ((choice == 'M') || (choice == 'm'))    {
+        cout << "Muffins added. $";
+        total += costMuffin;
+    }
+    else if ((choice == 'P') || (choice == 'p'))    {
+        cout << "Pastries added. $";
+        total += costPastry;
+    }
+    else if ((choice == 'B') || (choice == 'b'))    {
+        cout << "Bagels added. $";
+        total += costBagel;
+    }
+    else if ((choice == 'T') || (choice == 't'))    {
+        cout << "Toast added. $";
+        total += costToast;
+    }
+    else if ((choice == 'C') || (choice == 'c'))    {
+        cout << "Coffee added. $";
+        total += costCoffee;
+    }
+    else if ((choice == 'J') || (choice == 'j'))    {
+        cout << "Juice added. $";
+        total += costJuice;
+    }
+
+    return total;
+}
+
+
+
+
+int main ()  {
+    int userOption;
+    char userChar;
+    double total = 0.00;
+
+    printWelcomeMessage();
+    
+    cout << fixed << setprecision(2);
+    
+    printMenu();
+    cout << endl << ">>";
+    cin >> userOption;
+
+    printSubMenu(userOption);
+    cout << endl << ">>";
+    cin >> userChar;
+
+    // printSelection(userChar);
+
+           
+
+
+        
+       
+
+        
+        
+        
+    
+    //how to print running total?
+        
+
+
+
+    //exit program using 4 and only print end message?
+        
+    
+    
+    
+    
+    
+    
+    printEndMessage();
+
+    return 0;
+}
+
+void printWelcomeMessage () {
     cout << "Welcome to my Coffee Shop!" << endl << endl;
 }
 
@@ -39,58 +135,27 @@ void printEndMessage () {
 }
 
 void printMenu () {
-    //Menu options
+
+    //Main Menu options
     cout << "Please pick an option below:" << endl;
     cout << "1. Donuts/Muffins/Pastries" << endl;
     cout << "2. Bagels/Toast" << endl;
     cout << "3. Coffee/Juice" << endl;
     cout << "4. Quit" << endl << endl;
-}
 
-int readOption (int input)  {
-    //input checker
-    while ((input < 1) || (input > 4))  {
-        cin.clear();
-        cin.ignore(100, '\n');
-        cout << "Invalid option. Please choose 1-4: ";
-        cin >> input;
-        cout << endl;
-    }
-}
-
-//FIXME: how to add choice
-void printSelection (char choice)  {
-
-    if ((choice == 'D') || (choice == 'd')) {
-        cout << "Donuts added." << endl;
-    }
-    else if ((choice == 'M') || (choice == 'm'))    {
-        cout << "Muffins added." << endl;
-    }
-    else if ((choice == 'P') || (choice == 'p'))    {
-        cout << "Pastries added." << endl;
-    }
-    else if ((choice == 'B') || (choice == 'b'))    {
-        cout << "Bagels added." << endl;
-    }
-    else if ((choice == 'T') || (choice == 't'))    {
-        cout << "Toast added." << endl;
-    }
-    else if ((choice == 'C') || (choice == 'c'))    {
-        cout << "Coffee added." << endl;
-    }
-    else if ((choice == 'J') || (choice == 'j'))    {
-        cout << "Juice added." << endl;
-    }
-}
-
-void printSubMenu (int option)  {
     
+}
+
+int printSubMenu (int option)  {
+
+    //Submenu options
     if (option == 1)    {
         cout << "Would you like" << endl;
         cout << "D: Donuts $" << costDonut << endl;
         cout << "M: Muffins $" << costMuffin << endl;
         cout << "P: Pastries $" << costPastry << endl << endl;
+        cout << endl << ">>";
+        cin >> option;
         cout << endl;
     }
 
@@ -98,6 +163,8 @@ void printSubMenu (int option)  {
         cout << "Would you like" << endl;
         cout << "B: Bagels $" << costBagel << endl;
         cout << "T: Toast $" << costToast << endl << endl;
+        cout << endl << ">>";
+        cin >> option;
         cout << endl;
     }
      
@@ -105,50 +172,40 @@ void printSubMenu (int option)  {
         cout << "Would you like" << endl;
         cout << "C: Coffee $" << costCoffee << endl;
         cout << "J: Juice $" << costJuice << endl << endl;
+        cout << endl << ">>";
+        cin >> option;
         cout << endl;
     }
+
+    return option;
 }
 
-//how to incorporate running total? 
-int costFunction (int cost) {
-    int total = 0;
-    total += cost;
+int readOption()  {
+    int input;
 
-    return total;
-}
-
-int main()  {
-    int userOption;
-    char userChar;
-    int total;
-
-    printWelcome();
-    
-    cout << fixed << setprecision(2);
-    
-    while (userOption != 4) {
-        printMenu();
-        cin >> userOption;
-        readOption(userOption);
+    //input checker
+    while ((input < 1) || (input > 4))  {
+        cin.clear();
+        cin.ignore(2, '\n');
+        cout << "Invalid option. Please choose 1-4: ";
+        cin >> input;
         cout << endl;
-    
-        printSubMenu(userOption);
-        cin >> userChar;
-        cout << endl;
-        printSelection(userChar);
-    
-        //how to print running total?
-
-        //exit program using 4 and only print end message?
-        
-    
-    
     }
+
+    return input;
+}
+
+char readOptionChar(char choice)  {
     
-
-    printEndMessage();
-
-
-
-    return 0;
+    //input checker for char
+    while ((choice != 'D') && (choice != 'd') && (choice != 'M') 
+        && (choice != 'm') && (choice != 'P') && (choice != 'p') 
+        && (choice != 'B') && (choice != 'b') && (choice != 'T')
+        && (choice != 't') && (choice != 'C') && (choice != 'c')
+        && (choice != 'J') && (choice != 'j')) {
+        cin.clear();
+        cout << "Invalid option! Would you like Donuts (D, M, or P): ";
+        cin >> choice;
+        cout << endl;
+    }
 }
