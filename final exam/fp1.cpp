a7fb1911f846781a461baf08390b73dc68bb0276
erine4 <erin.egloff@pcc.edu>
2022-03-15 14:21:43 -0700

    manyissueswithinput

--- final exam/fp1.cpp
+++ final exam/fp1.cpp
@@ -15,46 +15,63 @@
 
 using namespace std;
 
-//Constant Variables
-
-
 //Declare Functions
+int inputChecker(int);
 
 
 int main()  {
     //declare variables
-    int input;
+    int input = 0;
     int max = 0;
 
     //opening message
     cout << "Welcome to my Final Exam Practice." << endl << endl;
 
     cout << "Enter positive whole numbers separated by newlines (0 to quit): ";
-    //input
-    cin >> input; 
+    
+
 
     do {
-        while ((input > max) || (input > 0)) {
+        
+        inputChecker(input);
+        
+
+        //max 
+        while ((input > max) && (input > 0)) {
             max = input;
         }
-    
-        cin >> input;
+
 
     } while (input != 0);
 
     
-    if (input == 0) {
+    while (input == 0) {
+        
         if (max <= 0)  {
-        cout << "No positive whole numbers were entered." << endl;
+            cout << "No positive whole numbers were entered." << endl;
         }   
         
         else if (max > 0) {
             cout << "The largest number you entered is " << max << endl;
         }
-
+        
+        break;
     }    
     
 
 
     return 0;
+}
+
+int inputChecker (int input)    {
+    cin >> input;
+
+    while (!input)  {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Bad input. Please enter a whole number: ";
+        cin >> input;
+    }
+    
+    return input;
 }
