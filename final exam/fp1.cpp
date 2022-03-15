cfa21e0f83ce965f34ad870a1e464370d8c5ec40
erine4 <erin.egloff@pcc.edu>
2022-03-15 08:53:18 -0700

    finalexamfile

---
+++ final exam/fp1.cpp
@@ -0,0 +1,38 @@
+/******************************************************************************
+# Author:           Erin Egloff
+# Assignment:       Final Exam
+# Date:             March 23, 2022
+# Description:      
+# Input:            
+# Output:           
+# Sources:          None
+#******************************************************************************/
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+//Constant Variables
+
+
+//Declare Functions
+
+
+int main()  {
+    //declare variables
+
+    //opening message
+
+
+    //input
+
+
+
+
+    //ending message
+
+    return 0;
+}
